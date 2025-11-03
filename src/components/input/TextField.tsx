7c43d3117c4a90bc68fe2aa48272717b60eb681c
phuoc <ptpc228864@gmail.com>
2025-11-03 09:19:15 +0700

    component

--- src/components/input/TextField.tsx
+++ src/components/input/TextField.tsx
@@ -14,20 +14,20 @@ const sizeCss = (size: 'md' | 'sm' | 'lg') => {
     switch (size) {
         case 'md': {
             return css`
-                padding: 8px;
+                padding: 10px 14px;
                 font-size: 14px;
             `
         }
         case 'sm': {
             return css`
                 padding: 6px;
-                font-size: 14px;
+                font-size: 12px;
             `
         }
         case 'lg': {
             return css`
                 padding: 14px;
-                font-size: 14px;
+                font-size: 16px;
             `
         }
     }
@@ -42,8 +42,19 @@ export const TextField = ({ placeholder, size = 'md', value, onChange, isFullWid
 }
 
 const baseCss = css`
-    outline: none;
-    border-radius: 5px;
-    border: 1px solid #ccc;
+    border-radius: 8px;
     min-width: 150px;
+    color: #0E0E21;
+    border: 1px solid #EAE9EE;
+    box-shadow: 0px 1px 2px 0px #1018280D;
+    transition: all .3s ease-out;
+    ::placeholder {
+        color: #948D86;
+    }
+    :hover, :focus {
+        border: 1px solid #7399FF;
+    }
+    :focus {
+        box-shadow: 0px 0px 0px 4px #7399FF40;
+    }
 `
